ad7b7ec23fd1dd3ba2e28c7d7797d1119c2aef8d
Sandeep <sandeep26bhasker@gmail.com>
2024-04-03 01:52:29 +0530

    creating virtual env

---
+++ init_setup.sh
@@ -0,0 +1,10 @@
+echo [$(date)]: "Start"
+echo [$(echo)]: "Creating the virtual environment with python v3.10"
+conda create --prefix ./env python==3.10 -y
+echo [$(echo)]: "Activating the environment"
+conda activate ./env
+echo [$(echo)]: "installing required dev packages"
+pip install -r requirements.txt
+echo [$(echo)]: "END"
+
+# Doesn't work on windows

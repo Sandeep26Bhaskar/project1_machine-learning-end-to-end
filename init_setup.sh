c58d777c7b49cf25e40e6256d8b362200e057610
Sandeep <sandeep26bhasker@gmail.com>
2024-04-03 01:57:30 +0530

    changes for echo [$(date)]

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "Start"
-echo [$(echo)]: "Creating the virtual environment with python v3.10"
+echo [$(date)]: "Creating the virtual environment with python v3.10"
 conda create --prefix ./env python==3.10 -y
-echo [$(echo)]: "Activating the environment"
+echo [$(date)]: "Activating the environment"
 conda activate ./env
-echo [$(echo)]: "installing required dev packages"
+echo [$(date)]: "installing required dev packages"
 pip install -r requirements.txt
-echo [$(echo)]: "END"
+echo [$(date)]: "END"
